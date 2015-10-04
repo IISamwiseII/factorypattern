4e0f9b9b179b221aa7b46c4a6d1fc7d6f76b205b
IISamwiseII <sam.pontefract@gmail.com>
2015-10-04 18:03:49 +0100

    main

---
+++ factorypattern/src/factorypattern/FactoryPatternMain.java
@@ -0,0 +1,12 @@
+package factorypattern;
+
+public class FactoryPatternMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Start of Example");
+		
+		System.out.println("End of Example");
+	}
+
+}
